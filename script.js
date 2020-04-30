df57096922a1456c97060e43930e8e0f03e9d656
Mateusz Choma <kontakt@coderoad.pl>
2020-04-30 17:21:40 +0200

    Circle added

---
+++ script.js
@@ -0,0 +1,8 @@
+const circle = document.createElement('div')
+
+circle.style.width = '100px'
+circle.style.height = '100px'
+circle.style.backgroundColor = 'blue'
+circle.style.borderRadius = '50%'
+
+document.body.appendChild(circle)
