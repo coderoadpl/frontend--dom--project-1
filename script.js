de59b9599b61e2d66b7a46b2b38609414a8d00bc
Mateusz Choma <kontakt@coderoad.pl>
2020-04-30 17:33:54 +0200

    Move functions added

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ function createCircle(containerSelector) {
 
     const circle = document.createElement('div')
 
+    circle.style.position = 'absolute'
+    circle.style.top = '0px'
+    circle.style.left = '0px'
     circle.style.width = '100px'
     circle.style.height = '100px'
     circle.style.backgroundColor = 'blue'
@@ -13,6 +16,28 @@ function createCircle(containerSelector) {
 
     container.appendChild(circle)
 
+    return circle
+
 }
 
-createCircle('body')
+function moveDown(deltaTop = 10) {
+    const currentTop = Number(circle.style.top.replace('px', ''))
+    circle.style.top = currentTop + deltaTop + 'px'
+}
+
+function moveRight(deltaLeft = 10) {
+    const currentLeft = Number(circle.style.left.replace('px', ''))
+    circle.style.left = currentLeft + deltaLeft + 'px'
+}
+
+function moveUp(deltaTop = 10) {
+    const currentTop = Number(circle.style.top.replace('px', ''))
+    circle.style.top = currentTop - deltaTop + 'px'
+}
+
+function moveLeft(deltaLeft = 10) {
+    const currentLeft = Number(circle.style.left.replace('px', ''))
+    circle.style.left = currentLeft - deltaLeft + 'px'
+}
+
+const circle = createCircle('body')
